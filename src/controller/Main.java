92eda18dd3b5a08439abdaf32c24f4bc0b608746
PLParson <plparson01@gmail.com>
2019-02-15 16:47:36 +0000

    main class to run the app

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,4 +1,11 @@
 package controller;
 
+import view.MainGui;
+
+import javax.swing.*;
+
 public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new MainGui());
+    }
 }
